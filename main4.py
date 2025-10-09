302a56b1ef472d6dd7ac8ceba765226a4f87c29a
2480451 <edouard@desgagne.org>
2025-10-09 13:27:37 -0400

    v10

--- main4.py
+++ main4.py
@@ -1,86 +1,5 @@
-# import tkinter as tk
-# from tkinter import ttk
-#
-# class Formulaire(tk.Tk):
-#     def __init__(self):
-#         super().__init__()
-#         self.title("Formulaire")
-#         self.geometry("800x500")
-#
-#         self.columnconfigure(0, weight=1)
-#         self.rowconfigure(0, weight=1)
-#
-#         content = tk.Frame(self)
-#         content.grid(row=0, column=0, sticky="nsew")
-#
-#         content.rowconfigure(0, weight=1)
-#         content.rowconfigure(1, weight=10)
-#         content.rowconfigure(2, weight=1)
-#         content.columnconfigure(0, weight=1)
-#
-#         header = tk.Frame(content)
-#         header.grid(row=0, column=0, sticky="nsew", padx=8, pady=2)
-#         header.rowconfigure(0, weight=1)
-#         header.columnconfigure(0, weight=1)
-#         header.columnconfigure(1, weight=2)
-#         header.columnconfigure(2, weight=3)
-#
-#         main = tk.Frame(content)
-#         main.grid(row=1, column=0, sticky="nsew", padx=8, pady=2)
-#         main.columnconfigure(0, weight=2)
-#         main.columnconfigure(1, weight=4)
-#         main.columnconfigure(2, weight=2)
-#         main.columnconfigure(3, weight=1)
-#         main.columnconfigure(4, weight=3)
-#         main.rowconfigure(0, weight=1)
-#         main.rowconfigure(1, weight=10)
-#
-#         footer = tk.Frame(content)
-#         footer.grid(row=2, column=0, sticky="nsew", padx=8, pady=2)
-#         footer.rowconfigure(0, weight=1)
-#         footer.columnconfigure(0, weight=1)
-#         footer.columnconfigure(1, weight=1)
-#         footer.columnconfigure(2, weight=1)
-#         footer.columnconfigure(3, weight=1)
-#
-#         self.titre_nom = ttk.Label(header, text="Nom")
-#         self.titre_nom.grid(row=0, column=0, sticky="nsew")
-#         self.titre_email = ttk.Label(header, text="Email")
-#         self.titre_email.grid(row=0, column=1, sticky="nsew")
-#         self.titre_age = ttk.Label(header, text="Âge")
-#         self.titre_age.grid(row=0, column=2, sticky="nsew")
-#
-#
-#         self.nom_entry = ttk.Entry(main)
-#         self.nom_entry.grid(row=0, column=0, sticky="ew", padx=(0,2))
-#         self.email_entry = ttk.Entry(main)
-#         self.email_entry.grid(row=0, column=1, sticky="ew", padx=(2,2))
-#         self.age_entry = ttk.Entry(main)
-#         self.age_entry.grid(row=0, column=2, sticky="ew", padx=(2,8))
-#         self.ajouter_btn = ttk.Button(main, text="Ajouter")
-#         self.ajouter_btn.grid(row=0, column=3, padx=2, sticky="e")
-#         self.supprimer_selection_btn = ttk.Button(main, text="Supprimer la sélection")
-#         self.supprimer_selection_btn.grid(row=0, column=4, padx=(2,0) , sticky="e")
-#
-#         self.tableau = ttk.Treeview(main, columns=("Nom", "Email", "Âge"), show="headings")
-#         self.tableau.heading("#1", text="Nom")
-#         self.tableau.heading("#2", text="Email")
-#         self.tableau.heading("#3", text="Âge")
-#         self.tableau.grid(columnspan=5, row=1, sticky="nsew")
-#
-#
-#         self.json_btn = ttk.Button(footer, text="Importer JSON")
-#         self.json_btn.grid(column=0, row=0, sticky="w")
-#         self.csv_btn = ttk.Button(footer, text="Importer CSV")
-#         self.csv_btn.grid(column=1, row=0, sticky="w")
-#         self.export_json_btn = ttk.Button(footer, text="Exporter JSON")
-#         self.export_json_btn.grid(column=2, row=0, sticky="e")
-#         self.export_csv_btn = ttk.Button(footer, text="Exporter CSV")
-#         self.export_csv_btn.grid(column=3, row=0, sticky="e")
-#
 
 import tkinter as tk
-from tkinter import ttk
 from tkinter import messagebox
 from tkinter import ttk, filedialog
 import json, csv
@@ -117,10 +36,10 @@ class Formulaire(tk.Tk):
         self.Age.grid(row=0, column=2, sticky="w", padx=20, pady=5)
 
         # inputs
-        self.Nom_Entry = ttk.Entry(header_content)
-        self.Nom_Entry.grid(row=1, column=0, sticky="ew", padx=(20, 5), pady=5)
-        self.Email_Entry = ttk.Entry(header_content, width=30)
-        self.Email_Entry.grid(row=1, column=1, sticky="ew", padx=5, pady=5)
+        self.nom_entry = ttk.Entry(header_content)
+        self.nom_entry.grid(row=1, column=0, sticky="ew", padx=(20, 5), pady=5)
+        self.email_entry = ttk.Entry(header_content, width=30)
+        self.email_entry.grid(row=1, column=1, sticky="ew", padx=5, pady=5)
         self.Age_Entry = ttk.Entry(header_content)
         self.Age_Entry.grid(row=1, column=2, sticky="ew", padx=5, pady=5)
 
@@ -163,8 +82,8 @@ class Formulaire(tk.Tk):
         self.Btn_Exporter_CSV.grid(row=0, column=4, sticky="e", padx=(5, 20), pady=5)
 
     def ajouter(self):
-        nom = self.Nom_Entry.get()
-        email = self.Email_Entry.get()
+        nom = self.nom_entry.get()
+        email = self.email_entry.get()
         age = self.Age_Entry.get()
         pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
         if not nom:
@@ -183,8 +102,8 @@ class Formulaire(tk.Tk):
             "email": email,
             "age": age
         })
-        self.Nom_Entry.delete(0, "end")
-        self.Email_Entry.delete(0, "end")
+        self.nom_entry.delete(0, "end")
+        self.email_entry.delete(0, "end")
         self.Age_Entry.delete(0, "end")
 
     def supprimer(self):
@@ -199,6 +118,7 @@ class Formulaire(tk.Tk):
     def exporter_json(self):
         if not self.rows:
             messagebox.showinfo("Exporter JSON", "Rien à exporter")
+            return
 
         path = filedialog.asksaveasfilename(title="Exporter JSON", defaultextension=".json", filetypes=(("Json", "*.json"), ("All files", "*.*")))
         if not path:
@@ -249,9 +169,52 @@ class Formulaire(tk.Tk):
 
 
     def exporter_csv(self):
-        pass
+        if not self.rows:
+            messagebox.showinfo("Exporter CSV", "Rien à exporter")
+            return
+
+        path = filedialog.asksaveasfilename(title="Exporter CSV", defaultextension=".csv",
+                                            filetypes=(("Csv", "*.csv"), ("All files", "*.*")))
+        if not path:
+            return
+        try:
+            with open(path, "w", encoding="utf-8") as f:
+                writer = csv.writer(f, delimiter=",")
+                writer.writerow(["nom", "email", "age"])
+                for r in self.rows:
+                    writer.writerow([r.nom, r.email, r.age])
+            messagebox.showinfo("Exporter CSV", "Exportation CSV complétée.")
+
+        except Exception as e:
+            messagebox.showerror("Échec exportation CSV", str(e))
 
     def importer_csv(self):
-        pass
+        path = filedialog.askopenfilename(title="Importer CSV", filetypes=(("Csv", "*.csv"), ("All files", "*.*")))
+        if not path:
+            return
+        try:
+            with open(path, "r", encoding="utf-8") as f:
+                reader = csv.reader(f, delimiter=",")
+                data = reader
+
+            if not isinstance(data, list):
+                raise ValueError("Le CSV doit contenir une liste d'objet.")
+            self.rows = []
+            for r in data:
+                if isinstance(r, dict):
+                    ligne = {}
+                    for k in FIELDS:
+                        ligne[k] = r.get(k, "")
+                    self.rows.append(ligne)
+            self.refresh()
+            messagebox.showinfo("Importer CSV", "Importation CSV complétée.")
+        except Exception as e:
+            messagebox.showerror("Échec d'importation CSV", str(e))
+
+
+
+
+
+
 if __name__ == "__main__":
     Formulaire().mainloop()
